b73d775ccd6158cd7d9fe86b9c61ef3a37f6ebdc
M1r1c1e <andersonmiracle111@gmail.com>
2024-03-17 15:18:12 -0700

    listing all states

---
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+""" listing all states from the database hbtn_0e_0_usa """
+import MySQLdb
+import sys
+
+if __name__ == "__main__":
+    dbCon = MySQLdb.connect(
+            user=sys.argv[1], 
+            passwd=sys.argv[2], 
+            database=sys.argv[3]
+    )
+    cursor = dbCon.cursor()
+    cursor.execute("SELECT * FROM states")
+    states = cursor.fetchall()
+
+    for state in states:
+        print(state)
+
+    cursor.close()
+    dbCon.close()
