e97ee2d0f42911cd58673818e48790b93cda0206
M1r1c1e <andersonmiracle111@.com>
2024-04-23 14:32:14 -0700

    wb scraping

--- 0x14-javascript-web_scraping/5-request_store.js
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -1,14 +1,14 @@
 #!/usr/bin/node
-// script that gets the contents of a webpage 
+// getting content from web page
 
 const request = require('request');
-const file_str = require('fs');
+const filefs = require('fs');
 
 request.get(process.argv[2], (error, response, body) => {
   if (error) {
     console.log(error);
   } else {
-    file_str.writeFile(process.argv[3], body, 'utf-8', (error) => {
+    filefs.writeFile(process.argv[3], body, 'utf-8', (error) => {
       if (error) {
         console.log(error);
       }
