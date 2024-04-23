af70688f4c7a4e210b915376c1d62df65cede948
M1r1c1e <andersonmiracle111@.com>
2024-04-23 14:27:02 -0700

    wb scraping

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -1,8 +1,8 @@
 #!/usr/bin/node
 
-const file_open = require('fs');
+const filefs = require('fs');
 
-file_open.writeFile(process.argv[2], process.argv[3], 'utf-8',
+filefs.writeFile(process.argv[2], process.argv[3], 'utf-8',
   function (err) {
     if (err) {
       console.log(err);
